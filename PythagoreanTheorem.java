dc34a8c9e472691cfd0fee79c99176c6745f136b
Nurishka <nurim98@gmail.com>
2014-05-14 22:05:05 +0600

    Pythagorean Theorem Solution

---
+++ PythagoreanTheorem.java
@@ -0,0 +1,19 @@
+/*
+ * File: PythagoreanTheorem.java
+ * Name: 
+ * Section Leader: 
+ * -----------------------------
+ * This file is the starter file for the PythagoreanTheorem problem.
+ */
+
+import acm.program.*;
+
+public class PythagoreanTheorem extends ConsoleProgram {
+	public void run() {
+		println("Enter values to compute Pythagorean theorem.");
+		int a = readInt("a: ");
+		int b = readInt("b: ");
+		double c = Math.sqrt(a * a + b * b);
+		println("c = " + c);
+	}
+}
