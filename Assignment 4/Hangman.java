7a4111c8cca4de38e81b168db792567c8d025e78
Nurishka <nurim98@gmail.com>
2014-06-17 23:48:09 +0600

    Assignment 4 Solution
    Adding Part 2 of Assignment 2

--- Assignment 4/Hangman.java
+++ Assignment 4/Hangman.java
@@ -5,24 +5,30 @@
  * Assignment #4.
  */
 
-import acm.graphics.*;
 import acm.program.*;
 import acm.util.*;
 
-import java.awt.*;
 
 public class Hangman extends ConsoleProgram {
 
+	public void init()
+	{
+	canvas = new HangmanCanvas();
+	add(canvas);
+	}
+	
     public void run() {
 		println("Welcome to Hangman!");
-		String wordToGuess = wordSet.getWord(rgen.nextInt(0, wordSet.getWordCount() - 1));
-		String dashedWord = toDashWord(wordToGuess);
-		
-		while(numGuesses != 0 || dashedWord.equals(wordToGuess))
+		String wordToGuess = wordSet.getWord(rgen.nextInt(0, wordSet.getWordCount() - 1)); // getting a random word from HangmanLexicon
+		String dashedWord = toDashWord(wordToGuess); // getting a dashed version of the word
+		canvas.reset();
+		while(numGuesses != 0) // until the player loses or wins
 			{
 			println("The word now looks like this: " + dashedWord);
+			canvas.displayWord(dashedWord);
 			println("You have " + numGuesses + " guesses left.");
-			while(true)
+			
+			while(true) // requests a letter from a player and checks whether it's a letter and only one letter, shows an error if not and requests again.
 			{
 				guessedChar = readLine("Your guess: ");
 				if (guessedChar.length() == 1 && Character.isLetter(guessedChar.charAt(0))) 
@@ -34,19 +40,27 @@ public class Hangman extends ConsoleProgram {
 					println("You should enter one letter.");
 				}
 			}
-				char ch = guessedChar.charAt(0);
-				if (Character.isLowerCase(ch))
+			
+				char ch = guessedChar.charAt(0); // converts the given string to a char
+				if (Character.isLowerCase(ch)) // converts a char into a uppercase if entered as lowercase
 				{
 					ch = Character.toUpperCase(ch);
 				}
-				dashedWord = checkGuessedChar(ch, wordToGuess, dashedWord);
+				dashedWord = checkGuessedChar(ch, wordToGuess, dashedWord); // updates the dashed word with a letter if guessed correctly, and returns the same dashedWord if not. Also decreases numGuesses if not a letter is an incorrect guess.
+				if (dashedWord.equals(wordToGuess))
+				{
+					canvas.displayWord(dashedWord); // if not added, last guessed letter wouldn't be shown on the canvas
+					break;
+				}
 			}
-			if (dashedWord.equals(wordToGuess))
+		
+			if (dashedWord.equals(wordToGuess)) // prints if won
 			{
 				println("You guessed the word: " + wordToGuess);
 				println("You win.");
+				canvas.youWin();
 			}
-			else if (numGuesses == 0)
+			else if (numGuesses == 0) // prints if lost
 			{
 				println("You're completely hung.");
 				println("The word was: " + wordToGuess);
@@ -54,7 +68,7 @@ public class Hangman extends ConsoleProgram {
 			}
 	}
     
-    private String toDashWord(String word)
+    private String toDashWord(String word) // gives the dashed version of the word
     {
     	String result = "";
     	for (int i = 0; i < word.length(); i++)
@@ -64,31 +78,33 @@ public class Hangman extends ConsoleProgram {
     	return result;
     }
     
-    private String checkGuessedChar(char ch, String wordToGuess, String dashedWord)
+    private String checkGuessedChar(char ch, String wordToGuess, String dashedWord) // updates the dashed word with a letter if guessed correctly, and returns the same dashedWord if not. Also decreases numGuesses if not a letter is an incorrect guess.
     {
-    	String result = "";
-    	for (int i = 0; i < wordToGuess.length(); i++)
+    	String result = dashedWord;
+    	for (int i = 0; i < wordToGuess.length(); i++) // changes the result if a letter is correctly guessed
     	{
     		if (wordToGuess.charAt(i) == ch)
     		{
-    			result = dashedWord.substring(0, i) + ch + dashedWord.substring(i + 1);
+    			result = result.substring(0, i) + ch + result.substring(i + 1);
     		}
     	}
-    	if (result.equals("") || result.equals(dashedWord))
+    	if (result.equals("") || result.equals(dashedWord)) // if result remained unchanged ("" maybe when guessing the first letter, dashedWord on any other attempt), that means the guess was incorrect
     	{
     		result = dashedWord;
     		println("There are no " + ch + "'s in the word.");
+    		canvas.noteIncorrectGuess(ch);
     		numGuesses--;
     	}
     	else
     	{
-    		println("That guess is correct");
+    		println("That guess is correct."); // if result changed, that means guess is correct
     	}
     	return result;
     }
 
-    private String guessedChar = "";
-    private int numGuesses = 8;
+    private String guessedChar = ""; // string entered by user
+    private int numGuesses = 8; // number of guesses
     private HangmanLexicon wordSet = new HangmanLexicon();
     private RandomGenerator rgen = new RandomGenerator();
+    private HangmanCanvas canvas;
 }
--- Assignment 4/HangmanCanvas.java
+++ Assignment 4/HangmanCanvas.java
@@ -4,13 +4,18 @@
  * This file keeps track of the Hangman display.
  */
 
+import java.awt.Color;
+
 import acm.graphics.*;
+import acm.util.*;
 
 public class HangmanCanvas extends GCanvas {
 
 /** Resets the display so that only the scaffold appears */
 	public void reset() {
-		/* You fill this in */
+		removeAll();
+		scaffold = new GLine(SCAFFOLD_X, SCAFFOLD_Y, SCAFFOLD_X, SCAFFOLD_Y - SCAFFOLD_HEIGHT);
+		add(scaffold);
 	}
 
 /**
@@ -19,7 +24,10 @@ public class HangmanCanvas extends GCanvas {
  * been guessed so far; unguessed letters are indicated by hyphens.
  */
 	public void displayWord(String word) {
-		/* You fill this in */
+		remove(wordBeingGuessed);
+		wordBeingGuessed = new GLabel(word, 50, getHeight() - 40);
+		wordBeingGuessed.setFont("Helvetica-24");
+		add(wordBeingGuessed);
 	}
 
 /**
@@ -29,9 +37,79 @@ public class HangmanCanvas extends GCanvas {
  * guesses that appears at the bottom of the window.
  */
 	public void noteIncorrectGuess(char letter) {
-		/* You fill this in */
+		remove(guessedChars);
+		wrongChars += letter;
+		guessedChars = new GLabel(wrongChars, 50, getHeight() - 20);
+		guessedChars.setFont("Helvetica-20");
+		add(guessedChars);
+		System.out.println(wrongChars.length());
+		switch(wrongChars.length())
+		{
+		case 1:
+			beam = new GLine(SCAFFOLD_X, SCAFFOLD_Y - SCAFFOLD_HEIGHT, SCAFFOLD_X + BEAM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT);
+			rope = new GLine(SCAFFOLD_X + BEAM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT, SCAFFOLD_X + BEAM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH);
+			head = new GOval(SCAFFOLD_X + BEAM_LENGTH - HEAD_RADIUS, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH, HEAD_RADIUS * 2, HEAD_RADIUS * 2);
+			add(beam);
+			add(rope);
+			add(head);
+			break;
+		case 2:
+			body = new GLine(SCAFFOLD_X + BEAM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + HEAD_RADIUS * 2, SCAFFOLD_X + BEAM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + BODY_LENGTH);
+			add(body);
+			break;
+		case 3:
+			leftUpperArm = new GLine(SCAFFOLD_X + BEAM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + HEAD_RADIUS * 2 + ARM_OFFSET_FROM_HEAD, SCAFFOLD_X + BEAM_LENGTH - UPPER_ARM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + HEAD_RADIUS * 2 + ARM_OFFSET_FROM_HEAD);
+			leftLowerArm = new GLine(SCAFFOLD_X + BEAM_LENGTH - UPPER_ARM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + HEAD_RADIUS * 2 + ARM_OFFSET_FROM_HEAD, SCAFFOLD_X + BEAM_LENGTH - UPPER_ARM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + HEAD_RADIUS * 2 + ARM_OFFSET_FROM_HEAD + LOWER_ARM_LENGTH);
+			add(leftUpperArm);
+			add(leftLowerArm);
+			break;
+		case 4:
+			rightUpperArm = new GLine(SCAFFOLD_X + BEAM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + HEAD_RADIUS * 2 + ARM_OFFSET_FROM_HEAD, SCAFFOLD_X + BEAM_LENGTH + UPPER_ARM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + HEAD_RADIUS * 2 + ARM_OFFSET_FROM_HEAD);
+			rightLowerArm = new GLine(SCAFFOLD_X + BEAM_LENGTH + UPPER_ARM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + HEAD_RADIUS * 2 + ARM_OFFSET_FROM_HEAD, SCAFFOLD_X + BEAM_LENGTH + UPPER_ARM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + HEAD_RADIUS * 2 + ARM_OFFSET_FROM_HEAD + LOWER_ARM_LENGTH);
+			add(rightUpperArm);
+			add(rightLowerArm);
+			break;
+		case 5:
+			leftHip = new GLine(SCAFFOLD_X + BEAM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + BODY_LENGTH, SCAFFOLD_X + BEAM_LENGTH - HIP_WIDTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + BODY_LENGTH);
+			leftLeg = new GLine(SCAFFOLD_X + BEAM_LENGTH - HIP_WIDTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + BODY_LENGTH, SCAFFOLD_X + BEAM_LENGTH - HIP_WIDTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + BODY_LENGTH + LEG_LENGTH);
+			add(leftHip);
+			add(leftLeg);
+			break;
+		case 6:
+			rightHip = new GLine(SCAFFOLD_X + BEAM_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + BODY_LENGTH, SCAFFOLD_X + BEAM_LENGTH + HIP_WIDTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + BODY_LENGTH);
+			rightLeg = new GLine(SCAFFOLD_X + BEAM_LENGTH + HIP_WIDTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + BODY_LENGTH, SCAFFOLD_X + BEAM_LENGTH + HIP_WIDTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + BODY_LENGTH + LEG_LENGTH);
+			add(rightHip);
+			add(rightLeg);
+			break;
+		case 7:
+			leftFoot = new GLine(SCAFFOLD_X + BEAM_LENGTH - HIP_WIDTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + BODY_LENGTH + LEG_LENGTH, SCAFFOLD_X + BEAM_LENGTH - HIP_WIDTH - FOOT_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + BODY_LENGTH + LEG_LENGTH);
+			add(leftFoot);
+			break;
+		case 8:
+			rightFoot = new GLine(SCAFFOLD_X + BEAM_LENGTH + HIP_WIDTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + BODY_LENGTH + LEG_LENGTH, SCAFFOLD_X + BEAM_LENGTH + HIP_WIDTH + FOOT_LENGTH, SCAFFOLD_Y - SCAFFOLD_HEIGHT + ROPE_LENGTH + BODY_LENGTH + LEG_LENGTH);
+			youLose.setFont("Helvetica-36");
+			youLose.setLocation(getWidth() / 2 - youLose.getWidth() / 2, (getHeight() - youLose.getAscent()) / 2);
+			youLose.setColor(Color.RED);
+			add(rightFoot);
+			add(youLose);
+			break;
+		}
+		
 	}
-
+	
+	public void youWin()
+	{
+		removeAll();
+		youWin.setFont("Helvetica-36");
+		youWin.setLocation(getWidth() / 2 - youWin.getWidth() / 2, (getHeight() - youWin.getAscent()) / 2);
+		while (true)
+		{
+		youWin.setColor(rgen.nextColor());
+		this.setBackground(rgen.nextColor());
+		add(youWin);
+		}
+	}
+	
 /* Constants for the simple version of the picture (in pixels) */
 	private static final int SCAFFOLD_HEIGHT = 360;
 	private static final int BEAM_LENGTH = 144;
@@ -44,5 +122,28 @@ public class HangmanCanvas extends GCanvas {
 	private static final int HIP_WIDTH = 36;
 	private static final int LEG_LENGTH = 108;
 	private static final int FOOT_LENGTH = 28;
-
+	private static final int SCAFFOLD_X = 70;
+	private static final int SCAFFOLD_Y = 390;
+	
+	private GLine scaffold;
+	private GLine beam;
+	private GLine rope;
+	private GOval head;
+	private GLine body;
+	private GLine rightUpperArm;
+	private GLine rightLowerArm;
+	private GLine leftUpperArm;
+	private GLine leftLowerArm;
+	private GLine rightHip;
+	private GLine leftHip;
+	private GLine rightLeg;
+	private GLine leftLeg;
+	private GLine rightFoot;
+	private GLine leftFoot;
+	private GLabel wordBeingGuessed = new GLabel("");
+	private GLabel guessedChars = new GLabel("");
+	private GLabel youWin = new GLabel("YOU WIN!");
+	private GLabel youLose = new GLabel("You lose.");
+	private String wrongChars = "";
+	private RandomGenerator rgen = new RandomGenerator();
 }
